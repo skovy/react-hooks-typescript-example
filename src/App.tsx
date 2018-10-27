70dff348c736ef5df3be0e7d9e93150aea9eceea
Spencer Miskoviak <smiskoviak@gmail.com>
2018-10-27 11:24:58 -0700

    Create a Counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import "./App.css";
+import Counter from "./Counter";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="app">
+          <Counter />
+        </div>
       </div>
     );
   }
---
+++ src/Counter.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./Counter.css";
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className="counter">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+
+export default Counter;
